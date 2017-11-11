a21899784faa1438fc62bcfbbda3c1298dec4633
wethtorang <sengwethtorang@gmail.com>
2017-11-11 11:37:10 +0700

    spring mvc

---
+++ spring-mvc/src/main/java/com/kshrd/HomeController.java
@@ -0,0 +1,19 @@
+package com.kshrd;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	public HomeController() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Home COntroller");
+	}
+	
+	@RequestMapping("/index")
+	public String hello() {
+		return "Hello";
+	}
+	
+}
